64f9c5738e42904415659d730d9d695080f53e1f
RomaRoof <41184185+RomaRoof@users.noreply.github.com>
2024-07-16 13:33:54 +0500

    не значительные правки в строке 20, 30, 31

--- MLParkinsonsAnalysis/parkinsos_analysis_ml_xgboost.py
+++ MLParkinsonsAnalysis/parkinsos_analysis_ml_xgboost.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from collections import Counter
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from xgboost import XGBClassifier
+
 
 # Считываем имена столбцов из .data файл
 def read_names(file_path):
@@ -15,8 +15,9 @@ def read_names(file_path):
                 column_names.append(line.split(':')[0].strip())
     return column_names
 
+
 # Путь к файлам
-data_file = 'D:\Learning\Founder\DSHomeWork\DataScientistWork\dataset\parkinsons\parkinsons.data'
+data_file = "D:\\Learning\\Founder\\DSHomeWork\\DataScientistWork\\dataset\\parkinsons\\parkinsons.data"
 # Adaptive path to /data for git
 # FILE_PATH = '/data'
 # data_file = pd.read_csv(f'{FILE_PATH}/parkinsons.data')
@@ -26,8 +27,8 @@ column_names = read_names(data_file)
 
 # Загружаем данные в DataFrame
 data = pd.read_csv(data_file)
-#print(data.head())
-#print(data.columns)
+# print(data.head())
+# print(data.columns)
 
 # Подготовка данных
 X = data.drop(columns=['name', 'status'])
@@ -45,7 +46,6 @@ model = XGBClassifier().fit(X_train, y_train)
 
 # Прогнозирование на тестовой выборке
 y_pred = model.predict(X_test)
-
 
 # Оценка точности модели
 accuracy = accuracy_score(y_test, y_pred)
