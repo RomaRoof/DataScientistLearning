c290caf6438f07d2942220f9decb24ad4d16e70c
RomaRoof <41184185+RomaRoof@users.noreply.github.com>
2024-07-22 13:28:04 +0500

    Чат-бот структурирован. добавлен исключения для git. Переименованы некоторые файлы.

--- chat_bot_tg/utils.py
+++ chat_bot_tg/utils.py
@@ -1,1 +1,29 @@
-# Структура квиза
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram import types
+from quiz_data import quiz_data
+from database import get_quiz_index
+
+def generate_options_keyboard(answer_options, right_answer):
+    builder = InlineKeyboardBuilder()
+
+    for option in answer_options:
+        builder.add(types.InlineKeyboardButton(
+            text=option,
+            callback_data="right_answer" if option == right_answer else "wrong_answer")
+        )
+
+    builder.adjust(1)
+    return builder.as_markup()
+
+async def get_question(message, user_id):
+    current_question_index = await get_quiz_index(user_id)
+    correct_index = quiz_data[current_question_index]['correct_option']
+    opts = quiz_data[current_question_index]['options']
+    kb = generate_options_keyboard(opts, opts[correct_index])
+    await message.answer(f"{quiz_data[current_question_index]['question']}", reply_markup=kb)
+
+async def new_quiz(message):
+    user_id = message.from_user.id
+    current_question_index = 0
+    await update_quiz_index(user_id, current_question_index)
+    await get_question(message, user_id)
