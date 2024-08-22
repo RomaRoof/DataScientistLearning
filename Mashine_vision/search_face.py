5e5033e381e5e8cf945a0f992513dffbb1c67283
RomaRoof <41184185+RomaRoof@users.noreply.github.com>
2024-08-22 21:48:31 +0500

    версия кода с отрисовкой прямоугольника

--- Mashine_vision/search_face.py
+++ Mashine_vision/search_face.py
@@ -1,6 +1,32 @@
 import cv2
+from matplotlib import pyplot
 
+from download_resurce import file_name
 
-img = cv2.imread('./1457583-2618007.jpg')
+img = cv2.imread(file_name)
 img_copy = img.copy()
 
+classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+bboxes = classifier.detectMultiScale(img)
+
+# формирование прямоугольника вокруг каждого обнаруженного лица
+for box in bboxes:
+    # формирование координат
+    x, y, width, height = box
+    x2, y2 = x + width, y + height
+    # рисование прямоугольников
+    cv2.rectangle(img_copy, (x, y), (x2, y2), (0, 0, 255), 2)
+
+fig, (ax1, ax2) = pyplot.subplots(1, 2, figsize=(15, 8))
+ax1.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+ax1.xaxis.set_ticks([])
+ax1.yaxis.set_ticks([])
+ax1.set_title('Исходное изображение')
+
+ax2.imshow(cv2.cvtColor(img_copy, cv2.COLOR_BGR2RGB))
+ax2.xaxis.set_ticks([])
+ax2.yaxis.set_ticks([])
+ax2.set_title('Распознанные лица')
+
+pyplot.show()
