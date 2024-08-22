acfb1289107a7452cb6530b7720b0c480f8a164d
RomaRoof <41184185+RomaRoof@users.noreply.github.com>
2024-08-22 21:47:50 +0500

    добавлена возможность проверки состояния файла (существует/не существует)

--- Mashine_vision/download_resurce.py
+++ Mashine_vision/download_resurce.py
@@ -1,7 +1,17 @@
+import os
 import requests
 
 url = 'https://www.film.ru/sites/default/files/people/1457583-2618007.jpg'
-response = requests.get(url)
+# Имя файла, под которым нужно сохранить изображение
+file_name = 'Ryan Reynolds.jpg'
 
-with open('friends.jpg', 'wb') as f:
-    f.write(response.content)
+# Проверка, существует ли файл
+if not os.path.exists(file_name):
+    # Если файл не существует, выполняется его загрузка
+    response = requests.get(url)
+    with open(file_name, 'wb') as f:
+        f.write(response.content)
+    print(f"Файл '{file_name}' загружен и сохранен.")
+else:
+    # Если файл существует, выводится сообщение
+    print(f"Файл '{file_name}' уже существует и не был загружен повторно.")
